2caa0d5b03566858f89d4e3a3d2673ade69cf222
Kassy0220 <shinbokutaro0220@gmail.com>
2024-09-28 17:42:38 +0900

    eslint-plugin-reactの設定を追加

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,32 @@
 import js from "@eslint/js";
+import react from "eslint-plugin-react"
 import globals from "globals";
 
 export default [
     js.configs.recommended,
+    react.configs.flat.recommended,
+    react.configs.flat['jsx-runtime'], /* React 17+ で必要 */
 
     {
+        files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
+        settings: {
+            react: {
+                version: "detect",
+            },
+        },
+        plugins: {
+            react
+        },
         rules: {
             "no-unused-vars": "warn",
             "no-undef": "warn"
         },
         languageOptions: {
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
             globals: {
                 ...globals.browser
             }
