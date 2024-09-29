c92e317b7f9aacb5a274367fb25ab84446cb1b81
Kassy0220 <shinbokutaro0220@gmail.com>
2024-09-29 16:02:13 +0900

    eslint.config.jsをPrettierで整形

--- eslint.config.js
+++ eslint.config.js
@@ -1,44 +1,44 @@
-import js from "@eslint/js";
-import react from "eslint-plugin-react"
-import reactHooks from "eslint-plugin-react-hooks"
-import prettier from "eslint-config-prettier"
-import jsxA11y from "eslint-plugin-jsx-a11y"
-import globals from "globals";
+import js from '@eslint/js'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import prettier from 'eslint-config-prettier'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
+import globals from 'globals'
 
 export default [
   js.configs.recommended,
   react.configs.flat.recommended,
-  react.configs.flat['jsx-runtime'], /* React 17+ で必要 */
+  react.configs.flat['jsx-runtime'] /* React 17+ で必要 */,
   jsxA11y.flatConfigs.recommended,
   prettier /* eslint-config-prettierは、上書きしたいプラグインより後に記述する必要がある */,
 
-    {
-        files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
-        settings: {
-            react: {
-                version: "detect",
-            },
+  {
+    files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      jsxA11y,
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-undef': 'warn',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
         },
-        plugins: {
-            react,
-            "react-hooks": reactHooks,
-            jsxA11y,
-        },
-        rules: {
-            "no-unused-vars": "warn",
-            "no-undef": "warn",
-            "react-hooks/rules-of-hooks": "error",
-            "react-hooks/exhaustive-deps": "warn",
-        },
-        languageOptions: {
-            parserOptions: {
-                ecmaFeatures: {
-                    jsx: true,
-                },
-            },
-            globals: {
-                ...globals.browser
-            }
-        }
-    }
-];
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+]
