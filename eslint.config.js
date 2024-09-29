2fdf337795549021395ce0c4063380d9b4f59186
Kassy0220 <shinbokutaro0220@gmail.com>
2024-09-29 15:45:03 +0900

    ESLintとPrettierが競合しないように、eslint-config-prettierを追加

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,16 @@
 import js from "@eslint/js";
 import react from "eslint-plugin-react"
 import reactHooks from "eslint-plugin-react-hooks"
+import prettier from "eslint-config-prettier"
 import jsxA11y from "eslint-plugin-jsx-a11y"
 import globals from "globals";
 
 export default [
-    js.configs.recommended,
-    react.configs.flat.recommended,
-    react.configs.flat['jsx-runtime'], /* React 17+ で必要 */
-    jsxA11y.flatConfigs.recommended,
+  js.configs.recommended,
+  react.configs.flat.recommended,
+  react.configs.flat['jsx-runtime'], /* React 17+ で必要 */
+  jsxA11y.flatConfigs.recommended,
+  prettier /* eslint-config-prettierは、上書きしたいプラグインより後に記述する必要がある */,
 
     {
         files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
