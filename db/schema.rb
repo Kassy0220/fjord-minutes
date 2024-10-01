09a59b1bd858dc6ed711a32d22de429f87a64ed6
Kassy0220 <shinbokutaro0220@gmail.com>
2024-10-01 18:48:07 +0900

    MinuteモデルをScaffoldで追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_30_215553) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_01_093422) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,4 +20,19 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_30_215553) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "minutes", force: :cascade do |t|
+    t.string "release_branch"
+    t.string "release_note"
+    t.text "other"
+    t.date "meeting_date"
+    t.date "next_meeting_date"
+    t.datetime "notified_at"
+    t.bigint "course_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_minutes_on_course_id"
+  end
+
+  add_foreign_key "minutes", "courses"
 end
