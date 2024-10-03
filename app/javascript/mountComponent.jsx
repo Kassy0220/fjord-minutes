df7022e067f1bc9dee0115361e7923360bb9949f
Kassy0220 <shinbokutaro0220@gmail.com>
2024-10-03 09:44:18 +0900

    Reactコンポーネントをレンダーするエントリポイントを追加

---
+++ app/javascript/mountComponent.jsx
@@ -0,0 +1,17 @@
+import React, { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+export default function mountComponent(id, Component) {
+  const element = document.getElementById(id)
+  if (element === null) {
+    return null
+  }
+
+  const root = createRoot(element)
+  const props = JSON.parse(element.getAttribute('data'))
+  root.render(
+    <StrictMode>
+      <Component {...props} />
+    </StrictMode>
+  )
+}
