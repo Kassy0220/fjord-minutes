35eb68485d4f04290a7995fecfa720f036e3e69a
Kassy0220 <shinbokutaro0220@gmail.com>
2024-10-03 10:06:26 +0900

    propsの型を定義して、ESLintの指摘箇所を修正

--- app/javascript/components/ReleaseInformation.jsx
+++ app/javascript/components/ReleaseInformation.jsx
@@ -1,7 +1,14 @@
+import PropTypes from 'prop-types'
+
 export default function ReleaseInformation({ minuteId, releaseBranch }) {
   return (
     <p>
       議事録({minuteId})のリリースブランチ: {releaseBranch}
     </p>
   )
+}
+
+ReleaseInformation.propTypes = {
+  minuteId: PropTypes.number,
+  releaseBranch: PropTypes.string,
 }
