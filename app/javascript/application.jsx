c4220a7616a43b823a3ed3f2a3bfd3cb547547e4
Kassy0220 <shinbokutaro0220@gmail.com>
2024-10-04 16:04:00 +0900

    リリースノート編集フォームを追加

--- app/javascript/application.jsx
+++ app/javascript/application.jsx
@@ -2,3 +2,4 @@ import mountComponent from './mountComponent.jsx'
 import ReleaseInformationForm from './components/ReleaseInformationForm.jsx'
 
 mountComponent('release_branch_form', ReleaseInformationForm)
+mountComponent('release_note_form', ReleaseInformationForm)
