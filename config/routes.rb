305b54f8bcf2cb143588aafe581cec6108594b92
Kassy0220 <shinbokutaro0220@gmail.com>
2024-10-06 08:34:30 +0900

    「話題にしたいこと」作成フォームを追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   resources :minutes
 
   namespace :api do
-    resources :minutes, only: [ :update ]
+    resources :minutes, only: [ :update ] do
+      resources :topics, only: [ :create ], module: :minutes
+    end
   end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
