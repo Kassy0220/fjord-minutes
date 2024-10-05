54ebae957d1e1179c9d576a624951c076796f781
Kassy0220 <shinbokutaro0220@gmail.com>
2024-10-06 07:43:29 +0900

    「話題にしたいこと」の一覧表示を追加

--- app/javascript/application.jsx
+++ app/javascript/application.jsx
@@ -1,9 +1,11 @@
 import mountComponent from './mountComponent.jsx'
 import ReleaseInformationForm from './components/ReleaseInformationForm.jsx'
+import TopicList from './components/TopicList.jsx'
 import OtherForm from './components/OtherForm.jsx'
 import NextMeetingDateForm from './components/NextMeetingDateForm.jsx'
 
 mountComponent('release_branch_form', ReleaseInformationForm)
 mountComponent('release_note_form', ReleaseInformationForm)
+mountComponent('topics', TopicList)
 mountComponent('other_form', OtherForm)
 mountComponent('next_meeting_date_form', NextMeetingDateForm)
