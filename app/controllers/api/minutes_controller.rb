138ae8c563c315b7fa63de04f000fa39ca736a01
Kassy0220 <shinbokutaro0220@gmail.com>
2024-10-09 18:24:22 +0900

    次回開催日の編集をリアルタイムで反映できるようにした

--- app/controllers/api/minutes_controller.rb
+++ app/controllers/api/minutes_controller.rb
@@ -4,7 +4,7 @@ class API::MinutesController < API::BaseController
 
     if minute.update(minute_params)
       render json: minute, status: :ok
-      MinuteChannel.broadcast_to(minute, body: { minute: minute.as_json(only: [ :release_branch, :release_note, :other ]) })
+      MinuteChannel.broadcast_to(minute, body: { minute: minute.as_json(only: [ :release_branch, :release_note, :other, :next_meeting_date ]) })
     else
       render json: { errors: minute.errors.full_messages }, status: :unprocessable_entity
     end
