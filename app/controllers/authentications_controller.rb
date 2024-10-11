5927b53264399e17d0aa1a77cf8ce33daef4c611
Kassy0220 <shinbokutaro0220@gmail.com>
2024-10-11 14:24:23 +0900

    OmniAuthのエラー時の処理を追加

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -15,7 +15,32 @@ class AuthenticationsController < Devise::OmniauthCallbacksController
     end
   end
 
+  # OmniAuthエラー時の処理は、Deviseで実装されている処理をそのまま追加
+  # https://github.com/heartcombo/devise/blob/72884642f5700439cc96ac560ee19a44af5a2d45/app/controllers/devise/omniauth_callbacks_controller.rb#L6
+  def passthru
+    render status: 404, plain: "Not found. Authentication passthru."
+  end
+
   def failure
+    set_flash_message! :alert, :failure, kind: OmniAuth::Utils.camelize(failed_strategy.name), reason: failure_message
     redirect_to root_path
+  end
+
+  protected
+
+  def failed_strategy
+    request.respond_to?(:get_header) ? request.get_header("omniauth.error.strategy") : request.env["omniauth.error.strategy"]
+  end
+
+  def failure_message
+    exception = request.respond_to?(:get_header) ? request.get_header("omniauth.error") : request.env["omniauth.error"]
+    error   = exception.error_reason if exception.respond_to?(:error_reason)
+    error ||= exception.error        if exception.respond_to?(:error)
+    error ||= (request.respond_to?(:get_header) ? request.get_header("omniauth.error.type") : request.env["omniauth.error.type"]).to_s
+    error.to_s.humanize if error
+  end
+
+  def translation_scope
+    "devise.omniauth_callbacks"
   end
 end
