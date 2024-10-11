d133b075adc4814a508f818a4f56123b1ad206b7
Kassy0220 <shinbokutaro0220@gmail.com>
2024-10-11 14:24:23 +0900

    OmniAuthのエラー時の処理を追加

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, ENV["AUTH_APP_ID"], ENV["AUTH_APP_SECRET"]
+  on_failure { |env| AuthenticationsController.action(:failure).call(env) }
 end
