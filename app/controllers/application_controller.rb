1b66b1163c7291005c1181faf641128737d02d6a
Kassy0220 <shinbokutaro0220@gmail.com>
2024-10-11 10:22:11 +0900

    ログイン後に表示されるダッシュボードページを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
 
   def after_sign_in_path_for(resource)
-    # 暫定的なパス
-    minutes_path
+    root_path
   end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    if current_member
+      render "member_dashboard"
+    else
+      render "index"
+    end
   end
 end
