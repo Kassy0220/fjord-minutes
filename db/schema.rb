21b0869664010e0af00b2588a3752bf643037e01
Kassy0220 <shinbokutaro0220@gmail.com>
2024-10-11 15:03:43 +0900

    Adminモデルを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_10_215545) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_11_055502) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+  end
 
   create_table "courses", force: :cascade do |t|
     t.string "name"
