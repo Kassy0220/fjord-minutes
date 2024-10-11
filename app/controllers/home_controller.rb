54a6fd5a3a34488c4a93b473ff9ea46bc4075e30
Kassy0220 <shinbokutaro0220@gmail.com>
2024-10-11 18:47:50 +0900

    認可機能を追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  skip_before_action :authenticate_development_member!, only: [ :index ]
+
   def index
     if current_development_member
       template = admin_login? ? "admin_dashboard" : "member_dashboard"
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -278,10 +278,12 @@ Devise.setup do |config|
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
   #
-  # config.warden do |manager|
-  #   manager.intercept_401 = false
-  #   manager.default_strategies(scope: :user).unshift :some_external_strategy
-  # end
+  config.warden do |manager|
+    # authenticate_XXXメソッドで認証されていないユーザーのリダイレクト先を設定(lib/custom_failure_app.rb)
+    manager.failure_app = CustomFailureApp
+    # manager.intercept_401 = false
+    # manager.default_strategies(scope: :user).unshift :some_external_strategy
+  end
 
   # ==> Mountable engine configurations
   # When using Devise inside an engine, let's call it `MyEngine`, and this engine
