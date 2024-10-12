3e02327b013632ed5590d8b814403056216eeec1
Kassy0220 <shinbokutaro0220@gmail.com>
2024-10-12 19:35:23 +0900

    開発環境にGitHub認証なしのログイン機能を追加

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :developer, fields: [ :email, :nickname, :image ], uid_field: :email if Rails.env.development?
   provider :github, ENV["AUTH_APP_ID"], ENV["AUTH_APP_SECRET"]
   on_failure { |env| AuthenticationsController.action(:failure).call(env) }
 end
