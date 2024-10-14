e9431db56c149825d8f45a625148dfa82de4466d
Kassy0220 <shinbokutaro0220@gmail.com>
2024-10-14 19:58:04 +0900

    議事録編集ページに無断欠席者表示機能を追加

--- app/controllers/api/minutes/attendances_controller.rb
+++ app/controllers/api/minutes/attendances_controller.rb
@@ -1,5 +1,7 @@
 class API::Minutes::AttendancesController < API::Minutes::ApplicationController
   def index
     @attendances = @minute.attendances.includes(:member)
+    # 休止機能を実装した際、無断欠席者から休止者は除外するようにする
+    @unexcused_absentees = Member.where(course_id: @minute.course_id).where.not(id: @attendances.pluck(:member_id))
   end
 end
