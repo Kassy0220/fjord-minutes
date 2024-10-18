4cf45044bcba8b369ffdd0a04b235d722d7c84c1
Kassy0220 <shinbokutaro0220@gmail.com>
2024-10-18 16:00:46 +0900

    出席を表示する時の並び順を指定

--- app/controllers/api/minutes/attendances_controller.rb
+++ app/controllers/api/minutes/attendances_controller.rb
@@ -1,7 +1,7 @@
 class API::Minutes::AttendancesController < API::Minutes::ApplicationController
   def index
-    @attendances = @minute.attendances.includes(:member)
+    @attendances = @minute.attendances.includes(:member).order(:member_id)
     # 休止機能を実装した際、無断欠席者から休止者は除外するようにする
-    @unexcused_absentees = Member.where(course_id: @minute.course_id).where.not(id: @attendances.pluck(:member_id))
+    @unexcused_absentees = Member.where(course_id: @minute.course_id).where.not(id: @attendances.pluck(:member_id)).order(:id)
   end
 end
