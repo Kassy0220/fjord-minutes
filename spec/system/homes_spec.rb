c4fc345bcb83536ef5fbf7dc68cd1d96cec41751
Kassy0220 <shinbokutaro0220@gmail.com>
2024-10-23 19:42:24 +0900

    トップページのテストを追加

---
+++ spec/system/homes_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe "Homes", type: :system do
+  scenario "user can access top page" do
+    FactoryBot.create(:rails_course)
+    FactoryBot.create(:front_end_course)
+
+    visit root_path
+    expect(page).to have_content 'Fjord Minutes'
+    expect(page).to have_button 'Railsエンジニアコースでログイン'
+    expect(page).to have_button 'フロントエンドエンジニアコースでログイン'
+  end
+end
