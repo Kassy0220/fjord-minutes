7d28f76f401e7edfd08e199e0f4573bcb72d30be
Kassy0220 <shinbokutaro0220@gmail.com>
2024-10-24 15:36:57 +0900

    議事録インスタンスからMarkdownを作成するクラスを追加

---
+++ app/models/markdown_builder.rb
@@ -0,0 +1,72 @@
+class MarkdownBuilder
+  TEMPLATE_PATH = "config/templates/minute.md"
+
+  def self.build(minute)
+    new(minute).build
+  end
+
+  def initialize(minute)
+    @minute = minute
+  end
+
+  def build
+    template = File.read(TEMPLATE_PATH)
+    minute_data = {
+      minute: @minute,
+      day_attendees:,
+      night_attendees:,
+      topics:,
+      next_date:,
+      absentees:
+    }
+    ERB.new(template).result_with_hash(minute_data)
+  end
+
+  private
+
+  def day_attendees
+    @minute.attendances.where(time: :day)
+                       .includes(:member)
+                       .order(:member_id)
+                       .pluck(:name)
+                       .map { |name| "    - #{name}" }
+                       .join("\n")
+  end
+
+  def night_attendees
+    @minute.attendances.where(time: :night)
+                       .includes(:member)
+                       .order(:member_id)
+                       .pluck(:name)
+                       .map { |name| "    - #{name}" }
+                       .join("\n")
+  end
+
+  def topics
+    @minute.topics.order(:created_at)
+                  .preload(:topicable)
+                  .map { |topic| "- #{topic.content}(#{topic.topicable.name})" }
+                  .join("\n")
+  end
+
+  def next_date
+    day_of_the_week = [ "日", "月", "火", "水", "木", "金", "土" ][@minute.next_meeting_date.wday]
+    meeting_date = "#{@minute.next_meeting_date.strftime('%Y年%m月%d日')}(#{day_of_the_week})"
+    return "- #{meeting_date}" unless HolidayJp.holiday?(@minute.next_meeting_date)
+
+    holiday_name = HolidayJp.between(@minute.next_meeting_date, @minute.next_meeting_date).first.name
+    <<~DATE.chomp
+      - #{meeting_date}
+        - 次回開催日は#{holiday_name}です。もしミーティングをお休みにする場合は、開催日を変更しましょう。
+    DATE
+  end
+
+  def absentees
+    @minute.attendances.where(status: :absent)
+                       .includes(:member)
+                       .order(:member_id)
+                       .pluck(:absence_reason, :progress_report, :name)
+                       .map { |absence_reason, progress_report, name| "- #{name}\n  - 欠席理由 : #{absence_reason}\n  - 進捗報告 : #{progress_report}" }
+                       .join("\n")
+  end
+end
