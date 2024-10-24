e6ad987a0492e0c4a0ca6cac49d7b23ad58d0617
Kassy0220 <shinbokutaro0220@gmail.com>
2024-10-24 19:13:10 +0900

    議事録詳細ページで、Markdownとプレビューを表示するようにした

---
+++ app/javascript/components/MarkdownPreview.jsx
@@ -0,0 +1,44 @@
+import { Tabs } from 'flowbite-react'
+import { marked } from 'marked'
+import DOMPurify from 'dompurify'
+import PropTypes from 'prop-types'
+
+export default function MarkdownPreview({ markdown }) {
+  const sanitizedHTML = { __html: DOMPurify.sanitize(marked.parse(markdown)) }
+
+  return (
+    <Tabs aria-label="Default tabs" variant="default" theme={customTheme}>
+      <Tabs.Item active title="Raw">
+        <pre className="p-4 border">{markdown}</pre>
+      </Tabs.Item>
+      <Tabs.Item title="Markdown">
+        <div
+          className="p-4 border markdown-body"
+          dangerouslySetInnerHTML={sanitizedHTML}
+        />
+      </Tabs.Item>
+    </Tabs>
+  )
+}
+
+const customTheme = {
+  base: 'flex flex-col gap-2',
+  tablist: {
+    tabitem: {
+      base: 'flex items-center justify-center rounded-t-lg p-4 text-sm font-medium first:ml-0 focus:outline-none focus:ring-2 focus:ring-gray-500 disabled:cursor-not-allowed',
+      variant: {
+        default: {
+          base: 'rounded-t-lg',
+          active: {
+            on: 'bg-gray-400 text-white dark:bg-gray-800 dark:text-cyan-500',
+            off: 'text-gray-500 hover:bg-gray-50 hover:text-gray-600 dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-gray-300',
+          },
+        },
+      },
+    },
+  },
+}
+
+MarkdownPreview.propTypes = {
+  markdown: PropTypes.string,
+}
