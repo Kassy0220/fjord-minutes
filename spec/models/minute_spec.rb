6c17619918039218979ca2431e5c2293c3d68def
Kassy0220 <shinbokutaro0220@gmail.com>
2024-10-27 13:48:09 +0900

    model specの指摘箇所を修正

--- spec/models/minute_spec.rb
+++ spec/models/minute_spec.rb
@@ -1,26 +1,26 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Minute, type: :model do
-  context '#already_finished?' do
-    before do
-      @minute = FactoryBot.build(:minute)
-    end
+  describe '#already_finished?' do
+    let(:minute) { FactoryBot.build(:minute) }
 
     it 'returns false if today is before the meeting date' do
-      travel_to @minute.meeting_date.days_ago(1) do
-        expect(@minute.already_finished?).to be false
+      travel_to minute.meeting_date.days_ago(1) do
+        expect(minute.already_finished?).to be false
       end
     end
 
     it 'returns false if today is the meeting date' do
-      travel_to @minute.meeting_date do
-        expect(@minute.already_finished?).to be false
+      travel_to minute.meeting_date do
+        expect(minute.already_finished?).to be false
       end
     end
 
     it 'returns true if today is after the meeting date' do
-      travel_to @minute.meeting_date.days_since(1) do
-        expect(@minute.already_finished?).to be true
+      travel_to minute.meeting_date.days_since(1) do
+        expect(minute.already_finished?).to be true
       end
     end
   end
