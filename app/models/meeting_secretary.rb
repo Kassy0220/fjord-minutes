81783b0ecdfecc604c2480b6a7f300e26331a792
Kassy0220 <shinbokutaro0220@gmail.com>
2024-10-29 18:14:20 +0900

    初回の議事録作成処理を追加

--- app/models/meeting_secretary.rb
+++ app/models/meeting_secretary.rb
@@ -13,6 +13,11 @@ class MeetingSecretary
   end
 
   def create_minute
+    if @course.minutes.none?
+      create_first_minute
+      return
+    end
+
     latest_minute = @course.minutes.order(:created_at).last
     unless latest_minute.already_finished?
       leave_log("create_minute, #{@course.name}, not_executed, latest meeting isn't finished.")
@@ -27,6 +32,35 @@ class MeetingSecretary
   end
 
   private
+
+  def create_first_minute
+    working_directory = GithubWikiManager.new(@course).working_directory
+    latest_meeting_date = get_latest_meeting_date_from_cloned_minutes(working_directory)
+    unless latest_meeting_date.before? Time.zone.today
+      leave_log("create_first_minute, #{@course.name}, not_executed, latest meeting isn't finished.")
+      return
+    end
+
+    meeting_date = get_next_meeting_date_from_cloned_minutes(latest_meeting_date, working_directory)
+    next_meeting_date = MeetingDateCalculator.next_meeting_date(meeting_date, @course.meeting_week)
+
+    @course.minutes.create!(meeting_date:, next_meeting_date:)
+    leave_log("create_first_minute, #{@course.name}, executed")
+  end
+
+  def get_latest_meeting_date_from_cloned_minutes(working_directory)
+    Dir.glob('ふりかえり・計画ミーティング*', base: working_directory).map do |filename|
+      _, year, month, day = *filename.match(/ふりかえり・計画ミーティング(\d{4})年(\d{2})月(\d{2})/)
+      Date.new(year.to_i, month.to_i, day.to_i)
+    end.max
+  end
+
+  def get_next_meeting_date_from_cloned_minutes(meeting_date, working_directory)
+    filename = "ふりかえり・計画ミーティング#{meeting_date.strftime('%Y年%m月%d日')}.md"
+    minute_content = File.read(File.join(working_directory, filename))
+    _, year, month, day = *minute_content.match(/# 次回のMTG\n\n- (\d{4})年(\d{2})月(\d{2})日/)
+    Date.new(year.to_i, month.to_i, day.to_i)
+  end
 
   def leave_log(message)
     Rails.logger.info(message)
