a8c4f30ee107b99b4d1973f1d9a42a793b30a5cb
Kassy0220 <shinbokutaro0220@gmail.com>
2024-10-31 08:19:21 +0900

    meeting_secretary内の変数名を修正

--- app/models/meeting_secretary.rb
+++ app/models/meeting_secretary.rb
@@ -28,14 +28,14 @@ class MeetingSecretary
   end
 
   def create_first_minute
-    working_directory = GithubWikiManager.new(@course).working_directory
-    latest_meeting_date = get_latest_meeting_date_from_cloned_minutes(working_directory)
+    cloned_repository_path = GithubWikiManager.new(@course).working_directory
+    latest_meeting_date = get_latest_meeting_date_from_cloned_minutes(cloned_repository_path)
     unless latest_meeting_date.before? Time.zone.today
       leave_log("create_first_minute, #{@course.name}, not_executed, latest meeting isn't finished.")
       return
     end
 
-    meeting_date = get_next_meeting_date_from_cloned_minutes(latest_meeting_date, working_directory)
+    meeting_date = get_next_meeting_date_from_cloned_minutes(latest_meeting_date, cloned_repository_path)
     next_meeting_date = MeetingDateCalculator.next_meeting_date(meeting_date, @course.meeting_week)
 
     new_minute = @course.minutes.create!(meeting_date:, next_meeting_date:)
@@ -45,16 +45,16 @@ class MeetingSecretary
 
   private
 
-  def get_latest_meeting_date_from_cloned_minutes(working_directory)
-    Dir.glob('ふりかえり・計画ミーティング*', base: working_directory).map do |filename|
+  def get_latest_meeting_date_from_cloned_minutes(repository_path)
+    Dir.glob('ふりかえり・計画ミーティング*', base: repository_path).map do |filename|
       _, year, month, day = *filename.match(/ふりかえり・計画ミーティング(\d{4})年(\d{2})月(\d{2})/)
       Date.new(year.to_i, month.to_i, day.to_i)
     end.max
   end
 
-  def get_next_meeting_date_from_cloned_minutes(meeting_date, working_directory)
+  def get_next_meeting_date_from_cloned_minutes(meeting_date, repository_path)
     filename = "ふりかえり・計画ミーティング#{meeting_date.strftime('%Y年%m月%d日')}.md"
-    minute_content = File.read(File.join(working_directory, filename))
+    minute_content = File.read(File.join(repository_path, filename))
     _, year, month, day = *minute_content.match(/# 次回のMTG\n\n- (\d{4})年(\d{2})月(\d{2})日/)
     Date.new(year.to_i, month.to_i, day.to_i)
   end
