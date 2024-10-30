765f2e0c3546be920893acdcbe3c9e365bf43834
Kassy0220 <shinbokutaro0220@gmail.com>
2024-10-31 07:46:55 +0900

    議事録作成処理を追加

---
+++ spec/models/meeting_secretary_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe MeetingSecretary, type: :model do
+  describe '#create_minute' do
+    let(:rails_course) { FactoryBot.create(:rails_course) }
+    let(:latest_minute) { FactoryBot.create(:minute, next_meeting_date: Date.new(2024, 11, 20), course: rails_course) }
+    let(:meeting_secretary) { described_class.new(rails_course) }
+
+    it 'create minute of next meeting' do
+      travel_to latest_minute.meeting_date + 1.day do
+        expect { meeting_secretary.create_minute }.to change(rails_course.minutes, :count).by(1)
+      end
+
+      created_minute = rails_course.minutes.last
+      expect(created_minute.meeting_date).to eq Date.new(2024, 11, 20)
+      expect(created_minute.next_meeting_date).to eq Date.new(2024, 12, 4)
+    end
+
+    it 'does not create minute if latest meeting is not finished' do
+      travel_to latest_minute.meeting_date do
+        expect { meeting_secretary.create_minute }.not_to change(rails_course.minutes, :count)
+      end
+    end
+  end
+end
