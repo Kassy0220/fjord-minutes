45fc326e268c8782447b96eb7f9a7b7c1e2e246e
Kassy0220 <shinbokutaro0220@gmail.com>
2024-10-31 21:14:39 +0900

    テストデータ内で使用する日付をTimeWithZoneクラスを使うように変更

--- spec/factories/minutes.rb
+++ spec/factories/minutes.rb
@@ -5,8 +5,8 @@ FactoryBot.define do
     release_branch { '' }
     release_note { '' }
     other { '' }
-    meeting_date { Date.new(2024, 10, 2) }
-    next_meeting_date { Date.new(2024, 10, 16) }
+    meeting_date { Time.zone.local(2024, 10, 2) }
+    next_meeting_date { Time.zone.local(2024, 10, 16) }
     notified_at { nil }
     association :course, factory: :rails_course
   end
