714952de72a831559d9d9d2196244eac90e4da42
Kassy0220 <shinbokutaro0220@gmail.com>
2024-11-01 13:36:27 +0900

    他人が作成した話題にしたいこと・心配事は編集できないことを確認するテストを追加

--- spec/factories/members.rb
+++ spec/factories/members.rb
@@ -9,5 +9,11 @@ FactoryBot.define do
     name { 'alice' }
     avatar_url { 'https://gyazo.com/40600d4c2f36e6ec49ec17af0ef610d3' }
     association :course, factory: :rails_course
+
+    trait :another_member do
+      email { 'bob@example.com' }
+      uid { 222_222 }
+      name { 'bob' }
+    end
   end
 end
