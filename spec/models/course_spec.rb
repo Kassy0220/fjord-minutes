07fc3eafb97b4b3159b27684b247a6c1b80a6d17
Kassy0220 <shinbokutaro0220@gmail.com>
2024-11-02 10:14:57 +0900

    コースごとに開催されたミーティングの年度を取得するメソッドを追加

---
+++ spec/models/course_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Course, type: :model do
+  describe '#meeting_years' do
+    let(:course) { FactoryBot.build(:rails_course) }
+
+    it 'returns all years which the meeting was held' do
+      FactoryBot.create(:minute, meeting_date: Time.zone.local(2024, 1, 1), course:)
+      FactoryBot.create(:minute, meeting_date: Time.zone.local(2025, 1, 1), course:)
+      FactoryBot.create(:minute, meeting_date: Time.zone.local(2026, 1, 1), course:)
+      expect(course.meeting_years).to eq [2024, 2025, 2026]
+    end
+  end
+end
