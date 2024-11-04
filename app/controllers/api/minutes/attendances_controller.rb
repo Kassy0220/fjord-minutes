7866964babb66be12cb1c87494a33e96575de8a2
Kassy0220 <shinbokutaro0220@gmail.com>
2024-11-04 18:06:56 +0900

    休止中のユーザーは、議事録編集ページに表示しないようにした

--- app/controllers/api/minutes/attendances_controller.rb
+++ app/controllers/api/minutes/attendances_controller.rb
@@ -3,7 +3,6 @@
 class API::Minutes::AttendancesController < API::Minutes::ApplicationController
   def index
     @attendances = @minute.attendances.includes(:member).order(:member_id)
-    # 休止機能を実装した際、無断欠席者から休止者は除外するようにする
-    @unexcused_absentees = Member.where(course_id: @minute.course_id).where.not(id: @attendances.pluck(:member_id)).order(:id)
+    @unexcused_absentees = Member.active.where(course_id: @minute.course_id).where.not(id: @attendances.pluck(:member_id)).order(:id)
   end
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -11,6 +11,9 @@ class Member < ApplicationRecord
   has_many :topics, as: :topicable, dependent: :destroy
   has_many :hibernations, dependent: :destroy
 
+  scope :active, -> { where.not(id: hibernated.pluck(:id)) }
+  scope :hibernated, -> { joins(:hibernations).where(hibernations: { finished_at: nil }) }
+
   def self.from_omniauth(auth, params)
     find_or_create_by(provider: auth.provider, uid: auth.uid) do |member|
       member.email = auth.info.email
