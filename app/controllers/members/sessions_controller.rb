c68bba7286dd13a490fd14600d63242ac1752a1c
Kassy0220 <shinbokutaro0220@gmail.com>
2024-11-04 09:34:55 +0900

    ログアウトした際に休止モデルを作成するようにした

--- app/controllers/members/sessions_controller.rb
+++ app/controllers/members/sessions_controller.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class Members::SessionsController < Devise::SessionsController
+  # Devise::SessionsController#destroyを元に作成
+  def destroy
+    member = current_member
+    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+    if signed_out
+      set_flash_message! :notice, :signed_out
+      member.hibernations.create!
+    end
+    respond_to_on_destroy
+  end
 end
