26ef3e6bafdc3d181443aa874a1cc6ecafee0cf6
Kassy0220 <shinbokutaro0220@gmail.com>
2024-11-07 13:08:28 +0900

    Member#all_attendancesで休止を反映させた場合のテストを追加

--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -28,16 +28,35 @@ RSpec.describe Member, type: :model do
     let(:absent_minute) { FactoryBot.create(:minute, meeting_date: Time.zone.local(2024, 10, 16), course: rails_course) }
     let!(:unexcused_absent_minute) { FactoryBot.create(:minute, meeting_date: Time.zone.local(2024, 11, 6), course: rails_course) }
 
-    it 'returns all attendances since the member sign up' do
-      FactoryBot.create(:minute, meeting_date: Time.zone.local(2024, 9, 18), course: rails_course)
+    before do
+      member.update!(created_at: Time.zone.local(2024, 10, 1))
       FactoryBot.create(:attendance, member:, minute: present_minute)
       FactoryBot.create(:attendance, :absence, member:, minute: absent_minute)
+    end
 
-      member.update!(created_at: Time.zone.local(2024, 10, 1))
+    it 'returns all attendances since the member sign up' do
+      FactoryBot.create(:minute, meeting_date: Time.zone.local(2024, 9, 18), course: rails_course)
       expected_attendances = [{ minute_id: present_minute.id, date: Date.new(2024, 10, 2), status: 'present', time: 'day', absence_reason: nil },
                               { minute_id: absent_minute.id, date: Date.new(2024, 10, 16), status: 'absent', time: nil, absence_reason: '体調不良のため。' },
                               { minute_id: unexcused_absent_minute.id, date: Date.new(2024, 11, 6), status: nil, time: nil, absence_reason: nil }]
       expect(member.all_attendances).to match_array(expected_attendances)
+    end
+
+    it 'returns attendances until the hibernation started if the member is hibernated' do
+      FactoryBot.create(:hibernation, member:, created_at: Time.zone.local(2024, 11, 1))
+
+      october_attendances = [{ minute_id: present_minute.id, date: Date.new(2024, 10, 2), status: 'present', time: 'day', absence_reason: nil },
+                             { minute_id: absent_minute.id, date: Date.new(2024, 10, 16), status: 'absent', time: nil, absence_reason: '体調不良のため。' }]
+      expect(member.hibernated?).to be true
+      expect(member.all_attendances).to match_array(october_attendances)
+    end
+
+    it 'attendances during hibernation period is treated as hibernation' do
+      FactoryBot.create(:hibernation, finished_at: Time.zone.local(2024, 11, 30), member:, created_at: Time.zone.local(2024, 11, 1))
+
+      attendance_during_hibernation = { minute_id: unexcused_absent_minute.id, date: Date.new(2024, 11, 6), status: 'hibernation', time: nil,
+                                        absence_reason: nil }
+      expect(member.all_attendances).to include(attendance_during_hibernation)
     end
   end
 end
