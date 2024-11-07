2048abe817dd03903a44be3cfdc3849a398fc04a
Kassy0220 <shinbokutaro0220@gmail.com>
2024-11-07 15:32:21 +0900

    出席を年度ごとに分けて表示するテストを追加

--- spec/system/members_spec.rb
+++ spec/system/members_spec.rb
@@ -24,6 +24,10 @@ RSpec.describe 'Members', type: :system do
       let(:rails_course) { FactoryBot.create(:rails_course) }
       let(:member) { FactoryBot.create(:member, course: rails_course) }
 
+      before do
+        login_as member
+      end
+
       scenario 'attendance is listed', :js do
         FactoryBot.create(:minute, meeting_date: Time.zone.local(2025, 1, 1), course: rails_course)
         FactoryBot.build_list(:minute, 3) do |minute|
@@ -35,7 +39,6 @@ RSpec.describe 'Members', type: :system do
         FactoryBot.create(:attendance, :night, member:, minute: rails_course.minutes.second)
         FactoryBot.create(:attendance, :absence, member:, minute: rails_course.minutes.third)
 
-        login_as member
         visit member_path(member)
         expect(page).to have_selector 'span[data-table-head="2025-01-01"]', text: '01/01'
         expect(page).to have_selector 'span[data-table-data="2025-01-01"]', text: '昼'
@@ -49,6 +52,31 @@ RSpec.describe 'Members', type: :system do
         expect(page).not_to have_selector 'div[data-testid="flowbite-tooltip"]', text: '体調不良のため。'
         find('span[data-table-data="2025-02-05"]').hover
         expect(page).to have_selector 'div[data-testid="flowbite-tooltip"]', text: '体調不良のため。'
+      end
+
+      scenario 'attendance is listed per year', :js do
+        FactoryBot.create(:minute, meeting_date: Time.zone.local(2024, 12, 18), course: rails_course)
+        FactoryBot.build_list(:minute, 2) do |minute|
+          minute.meeting_date = MeetingDateCalculator.next_meeting_date(rails_course.minutes.last.meeting_date, rails_course.meeting_week)
+          minute.course = rails_course
+          minute.save!
+        end
+        rails_course.minutes.each { |minute| FactoryBot.create(:attendance, member:, minute:) }
+
+        visit member_path(member)
+        expect(page).to have_selector 'div[data-attendances-year="2024"]'
+        expect(page).to have_selector 'div[data-attendances-year="2025"]'
+
+        within('div[data-attendances-year="2024"]') do
+          expect(page).to have_selector 'span[data-table-head="2024-12-18"]', text: '12/18'
+          expect(page).to have_selector 'span[data-table-data="2024-12-18"]', text: '昼'
+        end
+        within('div[data-attendances-year="2025"]') do
+          expect(page).to have_selector 'span[data-table-head="2025-01-01"]', text: '01/01'
+          expect(page).to have_selector 'span[data-table-data="2025-01-01"]', text: '昼'
+          expect(page).to have_selector 'span[data-table-head="2025-01-15"]', text: '01/15'
+          expect(page).to have_selector 'span[data-table-data="2025-01-15"]', text: '昼'
+        end
       end
     end
   end
