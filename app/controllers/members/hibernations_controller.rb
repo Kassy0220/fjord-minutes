7b548242c3e8ac1966f70b049613a7da3373a8ae
Kassy0220 <shinbokutaro0220@gmail.com>
2024-11-10 19:53:28 +0900

    重複してメンバーが休止中にならないように対策を追加

--- app/controllers/members/hibernations_controller.rb
+++ app/controllers/members/hibernations_controller.rb
@@ -2,9 +2,18 @@
 
 class Members::HibernationsController < Members::ApplicationController
   before_action :authenticate_admin!
+  before_action :prohibit_duplicate_hibernations
 
   def create
     @member.hibernations.create!
     redirect_to course_members_path(@member.course, status: 'hibernated'), notice: "#{@member.name}を休止中にしました"
+  end
+
+  private
+
+  def prohibit_duplicate_hibernations
+    return unless @member.hibernated?
+
+    redirect_to course_members_path(@member.course, status: 'hibernated'), alert: "#{@member.name}さんはすでに休止中です"
   end
 end
