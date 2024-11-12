4038d48e012249d65fa1848378f0c5039c3bcd12
Kassy0220 <shinbokutaro0220@gmail.com>
2024-11-12 11:11:44 +0900

    管理者のヘッダー表示のテストを追加

--- spec/system/homes_spec.rb
+++ spec/system/homes_spec.rb
@@ -13,24 +13,39 @@ RSpec.describe 'Homes', type: :system do
     expect(page).to have_button 'フロントエンドエンジニアコースでログイン'
   end
 
-  scenario 'header content changes whether the member logged in' do
-    member = FactoryBot.create(:member)
+  context 'with header' do
+    scenario 'header content changes whether the member logged in' do
+      member = FactoryBot.create(:member)
 
-    visit root_path
-    within('nav#header') do
-      expect(page).to have_link 'Fjord Minutes', href: root_path
-      expect(page).not_to have_link '議事録', href: course_minutes_path(member.course)
-      expect(page).not_to have_link 'メンバー', href: course_members_path(member.course)
-      expect(page).not_to have_selector("img[src$='#{member.avatar_url}']")
+      visit root_path
+      within('nav#header') do
+        expect(page).to have_link 'Fjord Minutes', href: root_path
+        expect(page).not_to have_link '議事録', href: course_minutes_path(member.course)
+        expect(page).not_to have_link 'メンバー', href: course_members_path(member.course)
+        expect(page).not_to have_selector("img[src$='#{member.avatar_url}']")
+      end
+
+      login_as member
+      visit root_path
+      within('nav#header') do
+        expect(page).to have_link 'Fjord Minutes', href: root_path
+        expect(page).to have_link '議事録', href: course_minutes_path(member.course)
+        expect(page).to have_link 'メンバー', href: course_members_path(member.course)
+        expect(page).to have_selector("img[src$='#{member.avatar_url}']")
+      end
     end
 
-    login_as member
-    visit root_path
-    within('nav#header') do
-      expect(page).to have_link 'Fjord Minutes', href: root_path
-      expect(page).to have_link '議事録', href: course_minutes_path(member.course)
-      expect(page).to have_link 'メンバー', href: course_members_path(member.course)
-      expect(page).to have_selector("img[src$='#{member.avatar_url}']")
+    scenario "admin's header displays the links to first course minutes and first course members" do
+      first_course = FactoryBot.create(:rails_course)
+      second_course = FactoryBot.create(:front_end_course)
+      admin = FactoryBot.create(:admin)
+      login_as_admin admin
+      within('nav#header') do
+        expect(page).to have_link '議事録', href: course_minutes_path(first_course)
+        expect(page).to have_link 'メンバー', href: course_members_path(first_course)
+        expect(page).not_to have_link '議事録', href: course_minutes_path(second_course)
+        expect(page).not_to have_link 'メンバー', href: course_members_path(second_course)
+      end
     end
   end
 end
