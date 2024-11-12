548de2869d8bafe39ae9eba3d2a130d0ae25ca1f
Kassy0220 <shinbokutaro0220@gmail.com>
2024-11-12 09:18:34 +0900

    ヘッダーのテストを追加

--- spec/system/homes_spec.rb
+++ spec/system/homes_spec.rb
@@ -12,4 +12,25 @@ RSpec.describe 'Homes', type: :system do
     expect(page).to have_button 'Railsエンジニアコースでログイン'
     expect(page).to have_button 'フロントエンドエンジニアコースでログイン'
   end
+
+  scenario 'header content changes whether the member logged in' do
+    member = FactoryBot.create(:member)
+
+    visit root_path
+    within('nav#header') do
+      expect(page).to have_link 'Fjord Minutes', href: root_path
+      expect(page).not_to have_link '議事録', href: course_minutes_path(member.course)
+      expect(page).not_to have_link 'メンバー', href: course_members_path(member.course)
+      expect(page).not_to have_selector("img[src$='#{member.avatar_url}']")
+    end
+
+    login_as member
+    visit root_path
+    within('nav#header') do
+      expect(page).to have_link 'Fjord Minutes', href: root_path
+      expect(page).to have_link '議事録', href: course_minutes_path(member.course)
+      expect(page).to have_link 'メンバー', href: course_members_path(member.course)
+      expect(page).to have_selector("img[src$='#{member.avatar_url}']")
+    end
+  end
 end
