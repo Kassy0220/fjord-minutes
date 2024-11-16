bb2a6020186fdafea694ff757e655802c258ae85
Kassy0220 <shinbokutaro0220@gmail.com>
2024-11-16 21:35:52 +0900

    ログアウト処理で、修了を選択した場合は修了日を保存するように変更

--- app/controllers/members/sessions_controller.rb
+++ app/controllers/members/sessions_controller.rb
@@ -2,13 +2,24 @@
 
 class Members::SessionsController < Devise::SessionsController
   # Devise::SessionsController#destroyを元に作成
+  before_action :prohibit_logout_without_reason
+
   def destroy
     member = current_member
     signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
     if signed_out
       set_flash_message! :notice, :signed_out
+      member.update!(completed_at: Time.zone.today) if params[:reason] == 'completed'
       member.hibernations.create!
     end
     respond_to_on_destroy
+  end
+
+  private
+
+  def prohibit_logout_without_reason
+    return unless params[:reason].nil?
+
+    redirect_to root_path, alert: t('errors.messages.without_reason')
   end
 end
