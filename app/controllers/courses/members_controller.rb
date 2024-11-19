1b32ec009097c70e754588bdb56c42ec4570a393
Kassy0220 <shinbokutaro0220@gmail.com>
2024-11-19 18:04:19 +0900

    メンバー一覧ページで、現役ユーザーと全てのユーザーを表示するようにした

--- app/controllers/courses/members_controller.rb
+++ app/controllers/courses/members_controller.rb
@@ -2,7 +2,7 @@
 
 class Courses::MembersController < Courses::ApplicationController
   def index
-    members = params[:status] == 'hibernated' ? Member.hibernated : Member.active
+    members = params[:status] == 'all' ? Member.all : Member.active
     members = Member.active unless admin_signed_in?
     @members = members.where(course: @course).order(:created_at)
   end
