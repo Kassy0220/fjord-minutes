5196410a411657d6f218da4e9a842e5752c8f6b0
Kassy0220 <shinbokutaro0220@gmail.com>
2024-11-28 07:32:44 +0900

    欠席の文言はヘルパーメソッドから導出する必要がないため、ヘルパーメソッドから削除

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -3,7 +3,6 @@
 module AttendancesHelper
   def attendance_status(status, time)
     return '---' if status.nil?
-    return '欠席' if status == 'absent'
 
     { 'day' => '昼', 'night' => '夜' }[time]
   end
