27b01bd23fd65613045d0cdb523e10a867b3aa4c
Kassy0220 <shinbokutaro0220@gmail.com>
2024-11-28 19:13:36 +0900

    議事録詳細ページのタブを他のページのタブと統一した

--- app/javascript/components/MinutePreview.jsx
+++ app/javascript/components/MinutePreview.jsx
@@ -1,47 +1,61 @@
-import { Tabs } from 'flowbite-react'
+import { useState } from 'react'
 import { marked } from 'marked'
 import DOMPurify from 'dompurify'
 import PropTypes from 'prop-types'
 
 export default function MinutePreview({ markdown }) {
+  const [selectedTab, setSelectedTab] = useState('markdown')
+
   const sanitizedHTML = {
     __html: DOMPurify.sanitize(marked.parse(markdown, [{ gfm: true }])),
   }
 
   return (
-    <Tabs aria-label="Default tabs" variant="default" theme={customTheme}>
-      <Tabs.Item active title="Markdown">
-        <pre id="raw_markdown" className="p-4 border">
-          {markdown}
-        </pre>
-      </Tabs.Item>
-      <Tabs.Item title="Preview">
-        <div
-          id="markdown_preview"
-          className="p-4 border markdown-body"
-          dangerouslySetInnerHTML={sanitizedHTML}
-        />
-      </Tabs.Item>
-    </Tabs>
-  )
-}
+    <div>
+      <div className="mb-8">
+        <ul className="flex flex-wrap text-sm text-center border-b border-gray-300 !list-none">
+          <li className="me-2">
+            <button
+              onClick={() => setSelectedTab('markdown')}
+              className={
+                selectedTab === 'markdown'
+                  ? 'active_large_tab_item'
+                  : 'large_tab_item'
+              }
+            >
+              Markdown
+            </button>
+          </li>
+          <li className="me-2">
+            <button
+              onClick={() => setSelectedTab('preview')}
+              className={
+                selectedTab === 'preview'
+                  ? 'active_large_tab_item'
+                  : 'large_tab_item'
+              }
+            >
+              Preview
+            </button>
+          </li>
+        </ul>
+      </div>
 
-const customTheme = {
-  base: 'flex flex-col gap-2',
-  tablist: {
-    tabitem: {
-      base: 'flex items-center justify-center rounded-t-lg p-4 me-2 border border-b-0 border-gray-300 text-sm font-medium',
-      variant: {
-        default: {
-          base: 'rounded-t-lg',
-          active: {
-            on: 'bg-blue-700 text-white font-bold',
-            off: 'text-gray-500 hover:bg-blue-50',
-          },
-        },
-      },
-    },
-  },
+      <div>
+        {selectedTab === 'markdown' ? (
+          <pre id="raw_markdown" className="p-4 border border-gray-300">
+            {markdown}
+          </pre>
+        ) : (
+          <div
+            id="markdown_preview"
+            className="p-4 border border-gray-300 markdown-body"
+            dangerouslySetInnerHTML={sanitizedHTML}
+          />
+        )}
+      </div>
+    </div>
+  )
 }
 
 MinutePreview.propTypes = {
