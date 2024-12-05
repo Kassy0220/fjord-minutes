5c76c2bfd53eda0fa2f4d84b11dea41140322174
Kassy0220 <shinbokutaro0220@gmail.com>
2024-12-05 09:41:55 +0900

    Action CableのコネクションにIDを追加

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -2,5 +2,30 @@
 
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_development_member
+
+    def connect
+      development_member = find_development_member
+      reject_unauthorized_connection unless development_member
+
+      self.current_development_member = development_member
+      Rails.logger.info("Action Cable connection from #{development_member.class}(id: #{development_member.id}) is established.")
+    end
+
+    def disconnect
+      Rails.logger.info("Action Cable connection from #{current_development_member.class}(id: #{current_development_member.id}) is closed.")
+    end
+
+    private
+
+    def find_development_member
+      if cookies.signed[:remember_admin_token]
+        Admin.serialize_from_cookie(*cookies.signed[:remember_admin_token])
+      elsif cookies.signed[:remember_member_token]
+        Member.serialize_from_cookie(*cookies.signed[:remember_member_token])
+      else
+        reject_unauthorized_connection
+      end
+    end
   end
 end
