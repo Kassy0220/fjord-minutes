f62b352c32cf8e7df77f49b01451c6dd5ce54375
Kassy0220 <shinbokutaro0220@gmail.com>
2024-12-06 15:20:18 +0900

    テスト内のログイン処理を共通化

--- spec/requests/minutes/topics_apis_spec.rb
+++ spec/requests/minutes/topics_apis_spec.rb
@@ -7,9 +7,12 @@ RSpec.describe 'Minutes::Topics API', type: :request do
   let(:member) { FactoryBot.create(:member) }
   let(:topic) { FactoryBot.create(:topic, :by_member, minute:, topicable: member) }
 
+  before do
+    sign_in member
+  end
+
   context 'when create topic' do
     it 'broadcast to subscriber when topic is created' do
-      sign_in member
       expect do
         post api_minute_topics_path(minute), params: { minute_id: minute.id, topic: { content: 'CIでテストが落ちています！' } }
       end.to have_broadcasted_to(minute).from_channel(MinuteChannel)
@@ -18,7 +21,6 @@ RSpec.describe 'Minutes::Topics API', type: :request do
 
   context 'when update topic' do
     it 'broadcast to subscriber when topic is updated' do
-      sign_in member
       expect do
         patch api_minute_topic_path(minute, topic), params: { topic: { content: '話題にしたいこと・心配事を更新しました' } }
       end.to have_broadcasted_to(minute).from_channel(MinuteChannel)
@@ -27,7 +29,6 @@ RSpec.describe 'Minutes::Topics API', type: :request do
 
   context 'when destroy topic' do
     it 'broadcast to subscriber when topic is destroyed' do
-      sign_in member
       expect do
         delete api_minute_topic_path(minute, topic)
       end.to have_broadcasted_to(minute).from_channel(MinuteChannel)
