af617f9d31fc7547be0307f4c59a0421487df5ce
Kassy0220 <shinbokutaro0220@gmail.com>
2024-12-06 18:20:24 +0900

    ブロードキャストのテストで、ブロードキャストで配信されるデータを確認するようにした

--- spec/requests/minutes/topics_apis_spec.rb
+++ spec/requests/minutes/topics_apis_spec.rb
@@ -13,6 +13,7 @@ RSpec.describe 'Minutes::Topics API', type: :request do
 
   context 'when create topic' do
     it 'broadcast to subscriber when topic is created' do
+      # post api_minute_topics_path(minute) で新しく作成されるデータを期待値に含めることができないので、ブロードキャストで配信されるデータまではテストしない
       expect do
         post api_minute_topics_path(minute), params: { minute_id: minute.id, topic: { content: 'CIでテストが落ちています！' } }
       end.to have_broadcasted_to(minute).from_channel(MinuteChannel)
@@ -21,17 +22,20 @@ RSpec.describe 'Minutes::Topics API', type: :request do
 
   context 'when update topic' do
     it 'broadcast to subscriber when topic is updated' do
+      broadcasted_data = [{ id: topic.id, content: '話題にしたいこと・心配事を更新しました', topicable_id: topic.topicable.id, topicable_type: 'Member',
+                            topicable: { name: topic.topicable.name } }.as_json]
       expect do
         patch api_minute_topic_path(minute, topic), params: { topic: { content: '話題にしたいこと・心配事を更新しました' } }
-      end.to have_broadcasted_to(minute).from_channel(MinuteChannel)
+      end.to have_broadcasted_to(minute).from_channel(MinuteChannel).with(body: { topics: broadcasted_data })
     end
   end
 
   context 'when destroy topic' do
     it 'broadcast to subscriber when topic is destroyed' do
+      broadcasted_data = []
       expect do
         delete api_minute_topic_path(minute, topic)
-      end.to have_broadcasted_to(minute).from_channel(MinuteChannel)
+      end.to have_broadcasted_to(minute).from_channel(MinuteChannel).with(body: { topics: broadcasted_data })
     end
   end
 end
--- spec/requests/minutes_apis_spec.rb
+++ spec/requests/minutes_apis_spec.rb
@@ -8,10 +8,12 @@ RSpec.describe 'Minutes API', type: :request do
 
   context 'when update minute' do
     it 'broadcast to subscriber when minute is updated' do
+      broadcasted_data = { release_branch: 'https://example.com/pull/1234', release_note: 'https://example.com/announcements/100', other: '連絡事項は特にありません',
+                           next_meeting_date: '2024-10-16' }.as_json
       sign_in admin
       expect do
-        patch api_minute_path(minute), params: { id: minute.id, minute: { release_branch: 'http://example.com/pull/1234' } }
-      end.to have_broadcasted_to(minute).from_channel(MinuteChannel)
+        patch api_minute_path(minute), params: { id: minute.id, minute: { release_branch: 'https://example.com/pull/1234' } }
+      end.to have_broadcasted_to(minute).from_channel(MinuteChannel).with(body: { minute: broadcasted_data })
     end
   end
 end
