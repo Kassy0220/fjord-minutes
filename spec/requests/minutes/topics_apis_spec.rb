2f7de4ce713b1e8a7876de97bc9a99add25ba453
Kassy0220 <shinbokutaro0220@gmail.com>
2024-12-06 19:20:51 +0900

    API::Minutes::TopicsControllerで話題にしたいことを作成・更新・削除するテストを追加

--- spec/requests/minutes/topics_apis_spec.rb
+++ spec/requests/minutes/topics_apis_spec.rb
@@ -5,13 +5,19 @@ require 'rails_helper'
 RSpec.describe 'Minutes::Topics API', type: :request do
   let!(:minute) { FactoryBot.create(:minute) }
   let(:member) { FactoryBot.create(:member) }
-  let(:topic) { FactoryBot.create(:topic, :by_member, minute:, topicable: member) }
+  let!(:topic) { FactoryBot.create(:topic, :by_member, minute:, topicable: member) }
 
   before do
     sign_in member
   end
 
   context 'when create topic' do
+    it 'create topic successfully' do
+      expect do
+        post api_minute_topics_path(minute), params: { minute_id: minute.id, topic: { content: 'CIでテストが落ちています！' } }
+      end.to change(minute.topics, :count).by(1)
+    end
+
     it 'broadcast to subscriber when topic is created' do
       # post api_minute_topics_path(minute) で新しく作成されるデータを期待値に含めることができないので、ブロードキャストで配信されるデータまではテストしない
       expect do
@@ -21,6 +27,12 @@ RSpec.describe 'Minutes::Topics API', type: :request do
   end
 
   context 'when update topic' do
+    it 'update topic successfully' do
+      expect do
+        patch api_minute_topic_path(minute, topic), params: { topic: { content: '話題にしたいこと・心配事を更新しました' } }
+      end.to change { Topic.find(topic.id).content }.from('gitのブランチ履歴がおかしくなってしまったので、どなたかペアプロをお願いしたいです').to('話題にしたいこと・心配事を更新しました')
+    end
+
     it 'broadcast to subscriber when topic is updated' do
       broadcasted_data = [{ id: topic.id, content: '話題にしたいこと・心配事を更新しました', topicable_id: topic.topicable.id, topicable_type: 'Member',
                             topicable: { name: topic.topicable.name } }.as_json]
@@ -31,6 +43,12 @@ RSpec.describe 'Minutes::Topics API', type: :request do
   end
 
   context 'when destroy topic' do
+    it 'destroy topic successfully' do
+      expect do
+        delete api_minute_topic_path(minute, topic)
+      end.to change(minute.topics, :count).by(-1)
+    end
+
     it 'broadcast to subscriber when topic is destroyed' do
       broadcasted_data = []
       expect do
