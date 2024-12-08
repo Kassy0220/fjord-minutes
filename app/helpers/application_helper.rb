122f3ada64ca919932bb966bbbd521423802f4cc
Kassy0220 <shinbokutaro0220@gmail.com>
2024-12-08 09:29:31 +0900

    各ページにmeta keywordsを追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
   def page_title(description)
     "#{description} | Fjord Minutes"
   end
+
+  def build_meta_tags(description)
+    { description:, keywords: 'チーム開発ミーティング, 議事録, フィヨルドブートキャンプ, FBC' }
+  end
 end
