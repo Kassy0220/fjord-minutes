9e589fa5d6fde3f84ef2ed582d9ada80e69f201f
Kassy0220 <shinbokutaro0220@gmail.com>
2024-12-09 17:50:52 +0900

    ogpの設定を追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,18 @@
 module ApplicationHelper
   def build_meta_tags(title: nil, description: nil)
     page_title = title.nil? ? 'Fjord Minutes' : "#{title} | Fjord Minutes"
-    { title: page_title, description:, keywords: 'チーム開発ミーティング, 議事録, フィヨルドブートキャンプ, FBC' }
+    {
+      title: page_title,
+      description:,
+      keywords: 'チーム開発ミーティング, 議事録, フィヨルドブートキャンプ, FBC',
+      og: {
+        url: 'https://fjord-minutes-eded74419c61.herokuapp.com',
+        type: 'website',
+        title: page_title,
+        description:,
+        site_name: 'Fjord Minutes',
+        image: image_url('ogp_image')
+      }
+    }
   end
 end
