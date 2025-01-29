05092831ec12cb0183cc6b2b1caedbffd6ebfe58
Kassy0220 <shinbokutaro0220@gmail.com>
2025-01-29 19:01:58 +0900

    議事録編集ページ内の出席登録・編集ボタンが目立つように位置とデザインを変更

--- app/javascript/components/AttendeesList.jsx
+++ app/javascript/components/AttendeesList.jsx
@@ -2,7 +2,7 @@ import useSWR from 'swr'
 import fetcher from '../fetcher.js'
 import PropTypes from 'prop-types'
 
-export default function AttendeesList({ minuteId, currentMemberId, isAdmin }) {
+export default function AttendeesList({ minuteId }) {
   const { data, error, isLoading } = useSWR(
     `/api/minutes/${minuteId}/attendances`,
     fetcher
@@ -16,53 +16,31 @@ export default function AttendeesList({ minuteId, currentMemberId, isAdmin }) {
       <h3>昼の部</h3>
       <ul id="day_attendees">
         {data.day_attendees.map((attendee) => (
-          <Attendee
-            key={attendee.attendance_id}
-            attendee={attendee}
-            currentMemberId={currentMemberId}
-            isAdmin={isAdmin}
-          />
+          <Attendee key={attendee.attendance_id} attendee={attendee} />
         ))}
       </ul>
       <h3>夜の部</h3>
       <ul id="night_attendees">
         {data.night_attendees.map((attendee) => (
-          <Attendee
-            key={attendee.attendance_id}
-            attendee={attendee}
-            currentMemberId={currentMemberId}
-            isAdmin={isAdmin}
-          />
+          <Attendee key={attendee.attendance_id} attendee={attendee} />
         ))}
       </ul>
     </>
   )
 }
 
-function Attendee({ attendee, currentMemberId, isAdmin }) {
+function Attendee({ attendee }) {
   return (
     <li key={attendee.attendance_id} className="mb-2">
       <a href={`https://github.com/${attendee.name}`}>{`@${attendee.name}`}</a>
-      {!isAdmin && currentMemberId === attendee.member_id && (
-        <a
-          href={`/attendances/${attendee.attendance_id}/edit`}
-          className="ps-4 no-underline hover:!no-underline"
-        >
-          <span className="button">出席編集</span>
-        </a>
-      )}
     </li>
   )
 }
 
 AttendeesList.propTypes = {
   minuteId: PropTypes.number,
-  currentMemberId: PropTypes.number,
-  isAdmin: PropTypes.bool,
 }
 
 Attendee.propTypes = {
   attendee: PropTypes.object,
-  currentMemberId: PropTypes.number,
-  isAdmin: PropTypes.bool,
 }
