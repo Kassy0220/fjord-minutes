004a40f585785f5e3b7483d32d53b10bc889ced8
Kassy0220 <shinbokutaro0220@gmail.com>
2025-02-01 14:10:05 +0900

    Attendanceモデルのpresent属性のデータ型を変更

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,17 +1,16 @@
 # frozen_string_literal: true
 
 class Attendance < ApplicationRecord
-  enum :status, { present: 0, absent: 1 }
   enum :session, { afternoon: 0, night: 1 }
 
   belongs_to :minute
   belongs_to :member
 
-  validates :status, presence: true
+  validates :present, inclusion: { in: [true, false] }
   validates :session, presence: true, if: proc { |attendance| attendance.present? }
-  validates :session, absence: true, if: proc { |attendance| attendance.absent? }
-  validates :absence_reason, presence: true, if: proc { |attendance| attendance.absent? }
-  validates :progress_report, presence: true, if: proc { |attendance| attendance.absent? }
+  validates :session, absence: true, unless: proc { |attendance| attendance.present? }
+  validates :absence_reason, presence: true, unless: proc { |attendance| attendance.present? }
+  validates :progress_report, presence: true, unless: proc { |attendance| attendance.present? }
   validates :absence_reason, absence: true, if: proc { |attendance| attendance.present? }
   validates :progress_report, absence: true, if: proc { |attendance| attendance.present? }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_31_120304) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_31_120737) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,7 +28,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_31_120304) do
   end
 
   create_table "attendances", force: :cascade do |t|
-    t.integer "present", null: false
+    t.boolean "present", null: false
     t.integer "session"
     t.string "absence_reason"
     t.text "progress_report"
