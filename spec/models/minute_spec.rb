75de7e4168963127b2a21b0e726cf81de2410d58
Kassy0220 <shinbokutaro0220@gmail.com>
2025-02-05 18:21:14 +0900

    テスト内の日付移動メソッドを、分かりやすいものに変更

--- spec/models/minute_spec.rb
+++ spec/models/minute_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Minute, type: :model do
     let(:minute) { FactoryBot.build(:minute) }
 
     it 'returns false if today is before the meeting date' do
-      travel_to minute.meeting_date.days_ago(1) do
+      travel_to minute.meeting_date.yesterday do
         expect(minute.already_finished?).to be false
       end
     end
@@ -19,7 +19,7 @@ RSpec.describe Minute, type: :model do
     end
 
     it 'returns true if today is after the meeting date' do
-      travel_to minute.meeting_date.days_since(1) do
+      travel_to minute.meeting_date.tomorrow do
         expect(minute.already_finished?).to be true
       end
     end
