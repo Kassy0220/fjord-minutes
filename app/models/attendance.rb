58d41fe7f81f386f1f0518f7912afa6bb88f7c5d
Kassy0220 <shinbokutaro0220@gmail.com>
2025-02-19 18:56:50 +0900

    Attendanceモデルにスコープを追加

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -6,6 +6,11 @@ class Attendance < ApplicationRecord
   belongs_to :minute
   belongs_to :member
 
+  scope :at_afternoon_session, -> { where(session: :afternoon) }
+  scope :at_night_session, -> { where(session: :night) }
+  scope :absent, -> { where(present: false) }
+  scope :with_members, -> { includes(:member) }
+
   validates :present, inclusion: { in: [true, false] }
   validates :session, presence: true, if: proc { |attendance| attendance.present? }
   validates :session, absence: true, unless: proc { |attendance| attendance.present? }
