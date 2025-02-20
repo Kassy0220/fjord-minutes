5b6c9c11df3eb93edeb19f6812134fa086db34ca
Kassy0220 <shinbokutaro0220@gmail.com>
2025-02-20 15:49:12 +0900

    WikiリポジトリのURLを返すメソッドをCourseモデルに移動

--- app/helpers/minutes_helper.rb
+++ app/helpers/minutes_helper.rb
@@ -2,7 +2,6 @@
 
 module MinutesHelper
   def github_wiki_url(minute)
-    repository_url = minute.course.name == 'Railsエンジニアコース' ? ENV.fetch('BOOTCAMP_WIKI_URL', nil) : ENV.fetch('AGENT_WIKI_URL', nil)
-    URI.join(repository_url.sub('.wiki.git', '/wiki/'), URI.encode_www_form_component(minute.title)).to_s
+    URI.join(minute.course.wiki_repository_url.sub('.wiki.git', '/wiki/'), URI.encode_www_form_component(minute.title)).to_s
   end
 end
