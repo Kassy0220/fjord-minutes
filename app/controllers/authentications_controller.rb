72478907009e92bb65dfaef263eec5584b63f35e
Kassy0220 <shinbokutaro0220@gmail.com>
2025-02-24 18:14:28 +0900

    ログイン時にキャンセルを選択した場合エラー画面が出る問題を修正

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -27,15 +27,8 @@ class AuthenticationsController < Devise::OmniauthCallbacksController
     end
   end
 
-  # OmniAuthエラー時の処理は、Deviseで実装されている処理をそのまま追加
-  # https://github.com/heartcombo/devise/blob/72884642f5700439cc96ac560ee19a44af5a2d45/app/controllers/devise/omniauth_callbacks_controller.rb#L6
-  def passthru
-    render status: :not_found, plain: 'Not found. Authentication passthru.'
-  end
-
   def failure
-    set_flash_message! :alert, :failure, kind: OmniAuth::Utils.camelize(failed_strategy.name), reason: failure_message
-    redirect_to root_path
+    redirect_to root_path, alert: t('.failure')
   end
 
   private
@@ -43,23 +36,5 @@ class AuthenticationsController < Devise::OmniauthCallbacksController
   def admin?(email)
     admin_emails = [ENV.fetch('KOMAGATA_EMAIL', 'no_email'), ENV.fetch('MACHIDA_EMAIL', 'no_email'), ENV.fetch('KASSY_EMAIL', 'no_email')]
     admin_emails.include? email
-  end
-
-  protected
-
-  def failed_strategy
-    request.respond_to?(:get_header) ? request.get_header('omniauth.error.strategy') : request.env['omniauth.error.strategy']
-  end
-
-  def failure_message
-    exception = request.respond_to?(:get_header) ? request.get_header('omniauth.error') : request.env['omniauth.error']
-    error   = exception.error_reason if exception.respond_to?(:error_reason)
-    error ||= exception.error        if exception.respond_to?(:error)
-    error ||= (request.respond_to?(:get_header) ? request.get_header('omniauth.error.type') : request.env['omniauth.error.type']).to_s
-    error.to_s.humanize if error # rubocop:disable Style/SafeNavigation
-  end
-
-  def translation_scope
-    'devise.omniauth_callbacks'
   end
 end
