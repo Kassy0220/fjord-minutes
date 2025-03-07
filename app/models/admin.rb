df0fe1367a2c2e7a9d702084c66f0d27976e9425
Kassy0220 <shinbokutaro0220@gmail.com>
2025-03-07 19:36:41 +0900

    アクセストークン作成処理をコントローラーからAdminモデルに移動した

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -17,4 +17,24 @@ class Admin < ApplicationRecord
       member.password = Devise.friendly_token[0, 20]
     end
   end
+
+  def initialize_github_credential(code)
+    params = {
+      client_id: ENV.fetch('GITHUB_APP_CLIENT_ID', nil),
+      client_secret: ENV.fetch('GITHUB_APP_CLIENT_SECRET', nil),
+      code:
+    }
+    response = Net::HTTP.post(
+      URI('https://github.com/login/oauth/access_token'),
+      URI.encode_www_form(params),
+      { Accept: 'application/json' }
+    )
+
+    raise "Error!, Status Code: #{response.code} #{response.message}, Response Body: #{response.body}" if response.code != '200'
+
+    response_body = JSON.parse(response.body)
+    raise "Error!, #{response_body['error']} : #{response_body['error_description']}" if response_body.key?('error')
+
+    create_github_credential!(access_token: response_body['access_token'], refresh_token: response_body['refresh_token'], expires_at: Time.zone.now + response_body['expires_in'])
+  end
 end
