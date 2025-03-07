a695f26d7091921d10dde9547779a0affbf29f88
Kassy0220 <shinbokutaro0220@gmail.com>
2025-03-07 10:35:46 +0900

    GitHub Wikiにpushする際、アクセストークンを作成しているが期限が切れている場合はトークンを更新するようにした

--- app/controllers/minutes/exports_controller.rb
+++ app/controllers/minutes/exports_controller.rb
@@ -4,7 +4,9 @@ class Minutes::ExportsController < Minutes::ApplicationController
   before_action :authenticate_admin!
 
   def create
-    MinuteGithubExporter.export_to_github_wiki(@minute)
+    current_admin.github_credential.update_access_token if current_admin.github_credential.expired?
+
+    MinuteGithubExporter.export_to_github_wiki(@minute, current_admin.github_credential.access_token)
     @minute.update!(exported: true) unless @minute.exported?
     redirect_to course_minutes_path(@minute.course), notice: 'GitHub Wikiに議事録を反映させました'
   end
