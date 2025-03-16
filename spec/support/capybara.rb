2635d553953303a7239a9c0729bbcbeb9819bc13
Kassy0220 <shinbokutaro0220@gmail.com>
2025-03-16 18:35:03 +0900

    selenium-webdriverの設定の書き方を修正

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -2,9 +2,9 @@
 
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    driven_by :selenium_chrome_headless do |options|
-      options.browser_version = '132.0.6834.159'
-      options.binary = '/opt/hostedtoolcache/setup-chrome/chromium/132.0.6834.159/x64/chrome'
-    end
+    driven_by :selenium, using: :chrome
+    options = Selenium::WebDriver::Options.chrome
+    options.browser_version = '132.0.6834.159'
+    options.binary = '/opt/hostedtoolcache/setup-chrome/chromium/132.0.6834.159/x64/chrome'
   end
 end
