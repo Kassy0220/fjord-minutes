5dfd1b00bd26b46ace0d44450e0a2b388c64aafd
Kassy0220 <shinbokutaro0220@gmail.com>
2025-03-16 18:20:45 +0900

    selenium-webdriverで使用するchromeのパスを指定

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -4,6 +4,7 @@ RSpec.configure do |config|
   config.before(:each, type: :system) do
     driven_by :selenium_chrome_headless do |options|
       options.browser_version = '132.0.6834.159'
+      options.binary = '/opt/hostedtoolcache/setup-chrome/chromium/132.0.6834.159/x64/chrome'
     end
   end
 end
