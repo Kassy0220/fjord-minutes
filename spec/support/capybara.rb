20396310b2f518457d6299916d009097174dc243
Kassy0220 <shinbokutaro0220@gmail.com>
2025-03-16 16:00:06 +0900

    SeleniumWebdriverでchromeのバージョンを指定するオプションを追加

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -2,6 +2,8 @@
 
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    driven_by :selenium_chrome_headless
+    driven_by :selenium_chrome_headless do |options|
+      options.browser_version = '132.0.6834.159'
+    end
   end
 end
