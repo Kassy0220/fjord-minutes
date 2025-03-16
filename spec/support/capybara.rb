e84081f43299f0ead5ef5fbe407ac539295bdae1
Kassy0220 <shinbokutaro0220@gmail.com>
2025-03-16 19:16:54 +0900

    chromeで使用するユーザーデータディレクトリを指定

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -3,7 +3,7 @@
 RSpec.configure do |config|
   config.before(:each, type: :system) do
     driven_by :selenium, using: :chrome
-    options = Selenium::WebDriver::Options.chrome
+    options = Selenium::WebDriver::Chrome::Options.new(args: ['user-data-dir=/tmp/temp_profile'])
     options.browser_version = '132.0.6834.159'
     options.binary = '/opt/hostedtoolcache/setup-chrome/chromium/132.0.6834.159/x64/chrome'
   end
