f3a152cc14c5e52d5f5422970831eca3a08ba5ae
Kassy0220 <shinbokutaro0220@gmail.com>
2025-03-16 20:38:36 +0900

    ユーザーディレクトリの指定を削除

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -3,7 +3,6 @@
 RSpec.configure do |config|
   config.before(:each, type: :system) do
     options = Selenium::WebDriver::Chrome::Options.new
-    options.add_argument('user-data-dir=/tmp/temp_profile')
     options.browser_version = '132.0.6834.159'
     options.binary = '/opt/hostedtoolcache/setup-chrome/chromium/132.0.6834.159/x64/chrome'
 
