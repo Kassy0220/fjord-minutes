c9540277d8c7c184b8f5a2262606cfb005879a50
Kassy0220 <shinbokutaro0220@gmail.com>
2025-03-16 19:36:33 +0900

    selenium-webdriverのoptionの設定を修正

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -2,9 +2,11 @@
 
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    driven_by :selenium, using: :chrome
-    options = Selenium::WebDriver::Chrome::Options.new(args: ['user-data-dir=/tmp/temp_profile'])
+    options = Selenium::WebDriver::Chrome::Options.new
+    options.add_argument('user-data-dir=/tmp/temp_profile')
     options.browser_version = '132.0.6834.159'
     options.binary = '/opt/hostedtoolcache/setup-chrome/chromium/132.0.6834.159/x64/chrome'
+
+    driven_by :selenium, using: :chrome, options: { browser: :chrome, options: options }
   end
 end
