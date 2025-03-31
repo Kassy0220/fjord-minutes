aae105e2bfc3bf0ab7a378fa5528f3a9ecdcdca2
Kassy0220 <shinbokutaro0220@gmail.com>
2025-03-31 18:52:05 +0900

    出席編集時にミーティングが終了していないか確認するコードの重複を修正

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,20 +1,17 @@
 # frozen_string_literal: true
 
 class AttendancesController < ApplicationController
-  def edit
-    attendance = current_member.attendances.find(params[:id])
-    redirect_to edit_minute_url(attendance.minute), alert: t('.failure') if attendance.minute.already_finished?
+  before_action :set_attendance, only: %i[edit update]
+  before_action :prohibit_edit_attendance_to_finished_meeting, only: %i[edit update]
 
-    @attendance_form = AttendanceForm.new(model: attendance)
+  def edit
+    @attendance_form = AttendanceForm.new(model: @attendance)
   end
 
   def update
-    attendance = current_member.attendances.find(params[:id])
-    redirect_to edit_minute_url(attendance.minute), alert: t('.failure') if attendance.minute.already_finished?
-
-    @attendance_form = AttendanceForm.new(model: attendance, **attendance_form_params)
+    @attendance_form = AttendanceForm.new(model: @attendance, **attendance_form_params)
     if @attendance_form.save
-      redirect_to edit_minute_path(attendance.minute), notice: t('.success')
+      redirect_to edit_minute_path(@attendance.minute), notice: t('.success')
     else
       render :edit, status: :unprocessable_entity
     end
@@ -24,5 +21,13 @@ class AttendancesController < ApplicationController
 
   def attendance_form_params
     params.require(:attendance_form).permit(:status, :absence_reason, :progress_report)
+  end
+
+  def set_attendance
+    @attendance = current_member.attendances.find(params[:id])
+  end
+
+  def prohibit_edit_attendance_to_finished_meeting
+    redirect_to edit_minute_url(@attendance.minute), alert: t('.failure') if @attendance.minute.already_finished?
   end
 end
