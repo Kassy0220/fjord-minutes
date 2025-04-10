7cfeea86bae13718f7379db7077b88ad3e2f8490
Kassy0220 <shinbokutaro0220@gmail.com>
2025-04-10 10:53:29 +0900

    MeetingDateCalculatorが担っていた処理をMeetingに移動
    ミーティングを作成する処理を修正

--- spec/support/meeting_support.rb
+++ spec/support/meeting_support.rb
@@ -2,10 +2,9 @@
 
 module MeetingSupport
   def create_meetings(course: nil, first_meeting_date: nil, count: nil)
-    FactoryBot.create(:meeting, date: first_meeting_date, next_date: MeetingDateCalculator.next_meeting_date(first_meeting_date, course.meeting_week), course:)
+    FactoryBot.create(:meeting, date: first_meeting_date, course:)
     FactoryBot.build_list(:meeting, count - 1) do |meeting|
       meeting.date = course.meetings.last.next_date
-      meeting.next_date = MeetingDateCalculator.next_meeting_date(course.meetings.last.next_date, course.meeting_week)
       meeting.course = course
       meeting.save!
     end
