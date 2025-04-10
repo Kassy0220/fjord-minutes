145d2a288b01f70cd8f6a0fc09f5b5932383e401
Kassy0220 <shinbokutaro0220@gmail.com>
2025-04-10 18:26:06 +0900

    MeetingSecretaryの議事録作成処理をCourseに移動

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 class Course < ApplicationRecord
+  include Rails.application.routes.url_helpers
+  CLONED_BOOTCAMP_WIKI_PATH = Rails.root.join('bootcamp_wiki_repository').freeze
+  CLONED_AGENT_WIKI_PATH = Rails.root.join('agent_wiki_repository').freeze
+  TEMPLATE_FOR_MINUTE_CREATION = 'config/templates/minute_creation_message.md'
+  TEMPLATE_FOR_TODAY_MEETING = 'config/templates/today_meeting_message.md'
+
   enum :meeting_week, { odd: 0, even: 1 }, suffix: true
 
   has_many :meetings, dependent: :restrict_with_exception
@@ -21,5 +27,55 @@ class Course < ApplicationRecord
 
   def discord_webhook_url
     { 'Railsエンジニアコース' => ENV.fetch('RAILS_COURSE_CHANNEL_URL', nil), 'フロントエンドエンジニアコース' => ENV.fetch('FRONT_END_COURSE_CHANNEL_URL', nil) }[name]
+  end
+
+  def create_next_meeting_and_minute
+    last_meeting_date, next_meeting_date = latest_meeting_schedule
+
+    unless last_meeting_date.before? Time.zone.today
+      Rails.logger.info("create_next_meeting_and_minute, #{name}, not_executed, last meeting isn't finished.")
+      return
+    end
+
+    next_meeting = meetings.create!(date: next_meeting_date)
+    next_meeting.create_minute!
+    Rails.logger.info("create_next_meeting_and_minute, #{name}, executed")
+    notification_message = ERB.new(File.read(TEMPLATE_FOR_MINUTE_CREATION))
+                              .result_with_hash({ discord_role_id:, course_name: name, meeting_date: I18n.l(next_meeting_date, format: :long), url: new_meeting_attendance_url(next_meeting) })
+    Discord::Notifier.message(notification_message, url: discord_webhook_url)
+  end
+
+  private
+
+  def latest_meeting_schedule
+    if meetings.exists?
+      latest_meeting = meetings.order(:date).last
+      return [latest_meeting.date, latest_meeting.next_date]
+    end
+
+    cloned_repository_path = name == 'Railsエンジニアコース' ? CLONED_BOOTCAMP_WIKI_PATH : CLONED_AGENT_WIKI_PATH
+    Git.clone(wiki_repository_url, cloned_repository_path) unless File.exist?(cloned_repository_path)
+
+    latest_meeting_date = get_latest_meeting_date_from_cloned_minutes(cloned_repository_path)
+    next_meeting_date = get_next_meeting_date_from_cloned_minutes(latest_meeting_date, cloned_repository_path)
+    [latest_meeting_date, next_meeting_date]
+  end
+
+  def get_latest_meeting_date_from_cloned_minutes(repository_path)
+    Dir.glob('ふりかえり・計画ミーティング*', base: repository_path).map do |filename|
+      _, year, month, day = *filename.match(/ふりかえり・計画ミーティング(\d{4})年(\d{2})月(\d{2})/)
+      Date.new(year.to_i, month.to_i, day.to_i) # Time.zone.today(返り値はDateクラス)と比較を行うために、Dateクラスでインスタンス化する
+    end.max
+  end
+
+  def get_next_meeting_date_from_cloned_minutes(date, repository_path)
+    filename = "ふりかえり・計画ミーティング#{I18n.l(date)}.md"
+    minute_content = File.read(File.join(repository_path, filename))
+    _, year, month, day = *minute_content.match(/# 次回のMTG\n\n- (\d{4})年(\d{2})月(\d{2})日/)
+    Date.new(year.to_i, month.to_i, day.to_i)
+  end
+
+  def discord_role_id
+    ENV.fetch('TEAM_MEMBER_ROLE_ID', nil).to_i
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_04_09_234411) do
+ActiveRecord::Schema[7.2].define(version: 2025_04_10_072039) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,6 +64,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_04_09_234411) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["course_id"], name: "index_meetings_on_course_id"
+    t.index ["date", "course_id"], name: "index_meetings_on_date_and_course_id", unique: true
   end
 
   create_table "members", force: :cascade do |t|
