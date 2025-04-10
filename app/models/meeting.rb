d7cedadb5654b3b1dfd3e713baf7a9bbf25ecde9
Kassy0220 <shinbokutaro0220@gmail.com>
2025-04-10 10:53:29 +0900

    MeetingDateCalculatorが担っていた処理をMeetingに移動
    ミーティングを作成する処理を修正

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,11 +1,46 @@
 # frozen_string_literal: true
 
 class Meeting < ApplicationRecord
+  DAY_OF_THE_WEEK_FOR_MEETING = { sun: 0, mon: 1, tue: 2, wed: 3, thu: 4, fri: 5, sat: 6 }[:wed]
+
+  before_create :set_next_date
+
   belongs_to :course
   has_one :minute, dependent: :destroy
   has_many :attendances, dependent: :destroy
 
   def already_finished?
     date.before?(Time.zone.today)
+  end
+
+  private
+
+  def set_next_date
+    self.next_date = date.day <= 14 ? date + 2.weeks : next_month_meeting_date
+  end
+
+  def next_month_meeting_date
+    year = date.month <= 11 ? date.year : date.year + 1
+    next_month = date.month <= 11 ? date.month + 1 : 1
+
+    meeting_days = all_meeting_days_in_month(year, next_month)
+    course.meeting_week == 'odd' ? Time.zone.local(year, next_month, meeting_days.first) : Time.zone.local(year, next_month, meeting_days.second)
+  end
+
+  def all_meeting_days_in_month(year, month)
+    meeting_days = []
+
+    first_day = Time.zone.local(year, month, 1)
+    last_day = first_day.end_of_month
+
+    meeting_day = first_day
+    meeting_day += 1.day until meeting_day.wday == DAY_OF_THE_WEEK_FOR_MEETING
+
+    while meeting_day <= last_day
+      meeting_days << meeting_day.day
+      meeting_day += 7.days
+    end
+
+    meeting_days
   end
 end
--- spec/models/meeting_spec.rb
+++ spec/models/meeting_spec.rb
@@ -3,6 +3,45 @@
 require 'rails_helper'
 
 RSpec.describe Meeting, type: :model do
+  describe 'sets next_date automatically on create' do
+    let(:rails_course) { FactoryBot.build(:rails_course, meeting_week: :odd) }
+    let(:front_end_course) { FactoryBot.build(:front_end_course, meeting_week: :even) }
+
+    context 'when date is within two weeks' do
+      it 'next_date is two weeks later' do
+        rails_course_meeting = described_class.create!(date: Time.zone.local(2024, 10, 2), course: rails_course)
+        expect(rails_course_meeting.next_date).to eq Date.new(2024, 10, 16)
+
+        front_end_course_meeting = described_class.create!(date: Time.zone.local(2024, 10, 9), course: front_end_course)
+        expect(front_end_course_meeting.next_date).to eq Date.new(2024, 10, 23)
+      end
+    end
+
+    context 'when date is after fifteenth' do
+      it 'next_date is the day of the first week of the next month when meeting week is odd' do
+        meeting = described_class.create!(date: Time.zone.local(2024, 10, 16), course: rails_course)
+        expect(meeting.next_date).to eq Date.new(2024, 11, 6)
+      end
+
+      it 'next_date is the day of the second week of the next month when meeting week is even' do
+        meeting = described_class.create!(date: Time.zone.local(2024, 10, 23), course: front_end_course)
+        expect(meeting.next_date).to eq Date.new(2024, 11, 13)
+      end
+    end
+
+    context 'when next_date is next year' do
+      it 'next_date is the day of the first week of next January when meeting week is odd' do
+        meeting = described_class.create!(date: Time.zone.local(2024, 12, 18), course: rails_course)
+        expect(meeting.next_date).to eq Date.new(2025, 1, 1)
+      end
+
+      it 'next_date is the day of the second week of next January when meeting week is even' do
+        meeting = described_class.create!(date: Time.zone.local(2024, 12, 25), course: front_end_course)
+        expect(meeting.next_date).to eq Date.new(2025, 1, 8)
+      end
+    end
+  end
+
   describe '#already_finished?' do
     let(:meeting) { FactoryBot.build(:meeting, course: FactoryBot.create(:rails_course)) }
 
