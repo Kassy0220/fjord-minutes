88165e2bdeeebb167406784d0b6fc83f64e33bc8
Kassy0220 <shinbokutaro0220@gmail.com>
2025-04-10 10:53:29 +0900

    MeetingDateCalculatorが担っていた処理をMeetingに移動
    ミーティングを作成する処理を修正

--- spec/factories/meetings.rb
+++ spec/factories/meetings.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :meeting do
     date { Time.zone.local(2024, 10, 2) }
-    next_date { Time.zone.local(2024, 10, 16) }
+    # next_date属性はモデル作成時に自動で日付がセットされるため、Factory内では指定しない
     notified_at { nil }
     association :course, factory: :rails_course
   end
