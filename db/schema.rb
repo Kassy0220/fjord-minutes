ed11da78abf59250c3b8075a916edab6fc22507e
Kassy0220 <shinbokutaro0220@gmail.com>
2025-04-10 09:03:17 +0900

    Minuteモデルの不要な属性を削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_04_09_224801) do
+ActiveRecord::Schema[7.2].define(version: 2025_04_09_234411) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -85,9 +85,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_04_09_224801) do
     t.string "release_branch", default: ""
     t.string "release_note", default: ""
     t.text "other", default: ""
-    t.date "meeting_date"
-    t.date "next_meeting_date"
-    t.datetime "notified_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "exported", default: false
--- spec/factories/minutes.rb
+++ spec/factories/minutes.rb
@@ -5,9 +5,6 @@ FactoryBot.define do
     release_branch { 'https://example.com/fjordllc/bootcamp/pull/1000' }
     release_note { 'https://example.com/announcements/100' }
     other { '連絡事項は特にありません' }
-    meeting_date { Time.zone.local(2024, 10, 2) }
-    next_meeting_date { Time.zone.local(2024, 10, 16) }
-    notified_at { nil }
     exported { false }
     association :meeting, factory: :meeting
   end
