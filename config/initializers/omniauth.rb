b4d5e2ec5989a4e0ea9a35787227b200fb5c08be
Kassy0220 <shinbokutaro0220@gmail.com>
2025-05-01 13:00:32 +0900

    GitHub認証時にユーザーのメールアドレスを参照できるようにスコープを追加

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer, fields: [ :email, :nickname, :image ], uid_field: :email if Rails.env.development?
-  provider :github, ENV["AUTH_APP_ID"], ENV["AUTH_APP_SECRET"]
+  provider :github, ENV["AUTH_APP_ID"], ENV["AUTH_APP_SECRET"], scope: 'user'
   on_failure { |env| OmniAuth::FailureEndpoint.new(env).redirect_to_failure }
 end
