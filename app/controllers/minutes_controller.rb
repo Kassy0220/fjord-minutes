b8e77c187d6d6bef2a93bdf410175de0637dca85
Kassy0220 <shinbokutaro0220@gmail.com>
2025-05-15 08:59:45 +0900

    所属していないコースの議事録編集ページにはアクセスできないようにした

--- app/controllers/minutes_controller.rb
+++ app/controllers/minutes_controller.rb
@@ -2,6 +2,7 @@
 
 class MinutesController < ApplicationController
   before_action :set_minute, only: %i[show edit]
+  before_action :prohibit_other_course_member_access, only: %i[edit]
 
   def show; end
 
@@ -13,5 +14,11 @@ class MinutesController < ApplicationController
 
   def set_minute
     @minute = Minute.find(params[:id])
+  end
+
+  def prohibit_other_course_member_access
+    return if admin_signed_in?
+
+    redirect_to root_path, alert: t('.failure.other_course') unless @minute.course == current_member.course
   end
 end
