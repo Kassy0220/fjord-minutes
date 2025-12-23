5b4850c86954325cf1fbdadce8cde48cb1290cad
Kassy0220 <shinbokutaro0220@gmail.com>
2025-12-23 17:07:36 +0900

    tailwindcssの設定を更新

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -15,15 +15,12 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
-    },
-    colors: {
-      markdownAnchorBlue: '#0969da',
+      colors: {
+        markdownAnchorBlue: '#0969da',
+      },
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
     require('flowbite/plugin'),
     flowbite.plugin(),
   ],
